a5682448be40b2f7195680fa0de14162a0ebebe7
Limjaewoo <ljw3944@naver.com>
2023-01-28 00:52:35 +0900

    Message properties - ko,en

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -10,4 +10,14 @@ public class ItemServiceApplication {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 
+	//	//스프링 메시시 소스 직접등록 방법 - 인터페이스 MessageSource의 구현체인 ResourceBundleMessageSource를 스프링 빈으로 등록하면된다.
+//	// 하지만 스프링 부트를 사용하면 직접등록할 필요없이 자동으로 스프링 빈으로 등록해준다. application.properties 확인
+//	@Bean
+//	public MessageSource messageSource() {
+//		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//		messageSource.setBasenames("messages", "errors");  //resources 밑에 있는 messages.properties파일을 읽는다.
+//		messageSource.setDefaultEncoding("utf-8");
+//		return messageSource;
+//	}
+
 }
